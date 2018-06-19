f08650586989803b69f27f14dae75c78bdc26dc4
Marcio <mpschiehl@outlook.com>
2018-06-19 10:49:28 -0300

    Desenvolvimento do Exercicio

--- Exercicio01.java
+++ Exercicio01.java
@@ -5,8 +5,10 @@
  */
 package Exercicio01;
 
+import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 /**
  *
@@ -14,8 +16,10 @@ import javax.swing.JLabel;
  */
 public class Exercicio01 implements InterfaceExercicio01{
 private JFrame jFrame;
-private JLabel jLabelNome,jLabelIdade,jLabelRaca,jLabelApelido,jLabelPreco,
-        jLabelDescricao;
+private JLabel jLabelNome,jLabelIdade,jLabelRaca,
+        jLabelApelido,jLabelPreco,jLabelDescricao;
+private JTextArea jTextAreaNome, jTextAreaIdade,jTextAreaRaca,
+        jTextAreaApelido,jTextAreaPreco,jTextAreaDescricao;
     public Exercicio01(){
      geralTela();
      instaciarComponentes();
@@ -27,8 +31,9 @@ private JLabel jLabelNome,jLabelIdade,jLabelRaca,jLabelApelido,jLabelPreco,
     @Override
     public void geralTela() {
        jFrame = new JFrame("Exercicio 01");
-       jFrame.setSize(660,440);
+       jFrame.setSize(620,440);
        jFrame.setLayout(null);
+       jFrame.getContentPane().setBackground(Color.decode("#ecefce"));
        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        jFrame.setLocationRelativeTo(null);
         
@@ -42,6 +47,12 @@ private JLabel jLabelNome,jLabelIdade,jLabelRaca,jLabelApelido,jLabelPreco,
        jFrame.add(jLabelNome);
        jFrame.add(jLabelPreco);
        jFrame.add(jLabelRaca);
+       jFrame.add(jTextAreaApelido);
+       jFrame.add(jTextAreaIdade);
+       jFrame.add(jTextAreaNome);
+       jFrame.add(jTextAreaPreco);
+       jFrame.add(jTextAreaRaca);
+       jFrame.add(jTextAreaDescricao);
     }
 
     @Override
@@ -52,26 +63,51 @@ private JLabel jLabelNome,jLabelIdade,jLabelRaca,jLabelApelido,jLabelPreco,
         jLabelNome= new JLabel("Nome");
         jLabelRaca= new JLabel("Ração");
         jLabelPreco = new JLabel("Preço");
+        jTextAreaApelido = new JTextArea();
+        jTextAreaIdade = new JTextArea();
+        jTextAreaNome = new JTextArea();
+        jTextAreaPreco = new JTextArea();
+        jTextAreaRaca = new JTextArea();
+        jTextAreaRaca = new JTextArea();
+        jTextAreaDescricao = new JTextArea();
+        
     }
 
     @Override
     public void gerarLocalizacoes() {
+        //Localização das Labels 
         jLabelNome.setLocation(10,10);
         jLabelIdade.setLocation(400,10);
-        jLabelRaca.setLocation(10,110);
-        jLabelApelido.setLocation(200,110);
-        jLabelPreco.setLocation(400,110);
+        jLabelRaca.setLocation(10,50);
+        jLabelApelido.setLocation(200,50);
+        jLabelPreco.setLocation(400,50);
         jLabelDescricao.setLocation(10,200);
+        //Localização das TextArea
+        jTextAreaNome.setLocation(10,30);
+        jTextAreaIdade.setLocation(400,30);
+        jTextAreaRaca.setLocation(10,70);
+        jTextAreaApelido.setLocation(200,70);
+        jTextAreaPreco.setLocation(400,70);
+        jTextAreaDescricao.setLocation(10,220);
+    
     }
 
     @Override
     public void gerarDimensoes() {
+        //dimensoes das labels
         jLabelNome.setSize(70,20);
         jLabelIdade.setSize(70,20);
         jLabelRaca.setSize(70,20);
         jLabelApelido.setSize(70,20);
         jLabelPreco.setSize(70,20);
         jLabelDescricao.setSize(70,20);
+        //dimensões TextArea
+        jTextAreaNome.setSize(340,20);
+        jTextAreaIdade.setSize(190,20);
+        jTextAreaRaca.setSize(140,20);
+        jTextAreaApelido.setSize(150,20);
+        jTextAreaPreco.setSize(190,20);
+        jTextAreaDescricao.setSize(580,88);
     }
     
 }
